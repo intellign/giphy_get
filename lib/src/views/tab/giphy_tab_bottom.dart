a14b222456bf3ad156c501035f3a3a4abf30bf79
intellign <ismailarashad@aol.com>
2024-12-18 11:52:52 -0500

    Update giphy_tab_bottom.dart

--- lib/src/views/tab/giphy_tab_bottom.dart
+++ lib/src/views/tab/giphy_tab_bottom.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:giphy_get/src/l10n/l10n.dart';
 
 class GiphyTabBottom extends StatelessWidget {
   const GiphyTabBottom({
@@ -7,12 +8,42 @@ class GiphyTabBottom extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.only(top: 10.0, bottom: 5.0),
-      child: Center(
-        child: _giphyLogo(context),
-      ),
-    );
+    final l = GiphyGetUILocalizations.labelsOf(context);
+
+    return true
+        ? Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Align(
+                  alignment: Alignment.centerLeft,
+                  child: Text(
+                    l.trendingOnGiphy,
+                    style: TextStyle(
+                        color: Colors.white.withOpacity(0.5),
+                        fontSize: 18,
+                        fontWeight: FontWeight.w500),
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Align(
+                    alignment: Alignment.centerRight,
+                    child: Image.asset(
+                        'assets/img/PoweredBy_200px-Black_HorizLogo.png',
+                        package: 'giphy_get',
+                        height: 20)),
+              )
+            ],
+          )
+        : Padding(
+            padding: const EdgeInsets.only(top: 10.0, bottom: 5.0),
+            child: Center(
+              child: _giphyLogo(context),
+            ),
+          );
   }
 
   Widget _giphyLogo(BuildContext context) {
