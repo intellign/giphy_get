87e69c07d092a581832d62e47c2c04b7893302f5
intellign <ismailarashad@aol.com>
2024-12-18 11:51:57 -0500

    Update giphy_tab_top.dart

--- lib/src/views/tab/giphy_tab_top.dart
+++ lib/src/views/tab/giphy_tab_top.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
 
 class GiphyTabTop extends StatefulWidget {
-  GiphyTabTop({Key? key}) : super(key: key);
+  final Color? topDragColor;
+  GiphyTabTop({Key? key, this.topDragColor}) : super(key: key);
 
   @override
   State<GiphyTabTop> createState() => _GiphyTabTopState();
@@ -10,11 +11,22 @@ class GiphyTabTop extends StatefulWidget {
 class _GiphyTabTopState extends State<GiphyTabTop> {
   @override
   Widget build(BuildContext context) {
-    return Container(
+    return Padding(
+      padding: const EdgeInsets.only(top: 10),
+      child: Container(
+        width: 40,
+        height: 5,
+        decoration: BoxDecoration(
+            color: widget.topDragColor ?? Colors.white.withOpacity(0.2),
+            borderRadius: const BorderRadius.all(Radius.circular(10))),
+      ),
+    );
+    /*Container(
       margin: EdgeInsets.symmetric(vertical: 8.0),
       width: 50,
       height: 2,
       color: Theme.of(context).textTheme.bodyLarge!.color!,
     );
+    */
   }
 }
