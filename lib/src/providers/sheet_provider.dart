ff01097d1677b87c9d6fcd4c1fa7d36f1917b264
intellign <ismailarashad@aol.com>
2024-12-18 22:32:51 -0500

    Update sheet_provider.dart

--- lib/src/providers/sheet_provider.dart
+++ lib/src/providers/sheet_provider.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 
 class SheetProvider extends ChangeNotifier {
   bool isExpanded = true;
-  static const double minExtent = 0.7;
+  static const double minExtent = 0.59;
   static const double maxExtent = .95;
   double initialExtent = 0.85;
   /* double _initialExtent = minExtent;
