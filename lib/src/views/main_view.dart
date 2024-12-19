fab16c7d73cef795917c97b90982832032e12736
intellign <ismailarashad@aol.com>
2024-12-18 22:33:12 -0500

    Update main_view.dart

--- lib/src/views/main_view.dart
+++ lib/src/views/main_view.dart
@@ -1,3 +1,4 @@
+import 'dart:ui' as ui;
 import 'package:flutter/material.dart';
 import 'package:giphy_get/giphy_get.dart';
 import 'package:giphy_get/src/providers/sheet_provider.dart';
@@ -7,18 +8,29 @@ import 'package:giphy_get/src/views/tab/giphy_tab_bottom.dart';
 import 'package:giphy_get/src/views/tab/giphy_tab_top.dart';
 import 'package:giphy_get/src/views/tab/giphy_tab_view.dart';
 import 'package:provider/provider.dart';
+import 'package:giphy_get/src/providers/app_bar_provider.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:giphy_get/src/providers/tab_provider.dart';
+import 'package:flutter/rendering.dart';
 
 class MainView extends StatefulWidget {
   MainView({
     Key? key,
+    required this.sharedPreferences,
     this.showEmojis = true,
     this.showGIFs = true,
     this.showStickers = true,
+    this.whiteBackground = false,
+    this.topDragColor,
     this.tabTopBuilder,
     this.tabBottomBuilder,
     this.searchAppBarBuilder,
+    this.addMediaTopWidget,
   }) : super(key: key);
-
+  final SharedPreferences sharedPreferences;
+  final Widget? addMediaTopWidget;
+  final Color? topDragColor;
+  final bool whiteBackground;
   final bool showGIFs;
   final bool showStickers;
   final bool showEmojis;
@@ -41,18 +53,42 @@ class _MainViewState extends State<MainView>
   // Tab Controller
   late TabController _tabController;
 
+  int lastGiphyTabSaved = 0;
+
+  late TextEditingController textEditingController;
+  FocusNode _focusNode = FocusNode();
   @override
   void initState() {
     super.initState();
 
+    textEditingController = new TextEditingController(
+        text: Provider.of<AppBarProvider>(context, listen: false).queryText);
+
+    lastGiphyTabSaved =
+        widget.sharedPreferences.getInt("lastGiphyTabSaved") ?? 0;
+
+    int currentFeaturesLength = [
+      widget.showGIFs,
+      widget.showEmojis,
+      widget.showStickers,
+    ].where((isShown) => isShown).length;
+
+    if (lastGiphyTabSaved > currentFeaturesLength) {
+      lastGiphyTabSaved = 0;
+    }
+
     _tabController = TabController(
-      length: [
-        widget.showGIFs,
-        widget.showEmojis,
-        widget.showStickers,
-      ].where((isShown) => isShown).length,
+      initialIndex: lastGiphyTabSaved,
+      length: currentFeaturesLength,
       vsync: this,
     );
+    WidgetsBinding.instance.addPostFrameCallback((timeStamp) async {
+      final _tabProvider = Provider.of<TabProvider>(context, listen: false);
+
+      if (widget.addMediaTopWidget != null) {
+        _tabProvider.setShowAddTopMediaWidgets(true);
+      }
+    });
   }
 
   @override
@@ -65,6 +101,7 @@ class _MainViewState extends State<MainView>
   @override
   void dispose() {
     super.dispose();
+    widget.sharedPreferences.setInt("lastGiphyTabSaved", _tabController.index);
   }
 
   @override
@@ -72,42 +109,165 @@ class _MainViewState extends State<MainView>
     return _draggableScrollableSheet();
   }
 
-  Widget _draggableScrollableSheet() => DraggableScrollableSheet(
-      expand: _sheetProvider.isExpanded,
-      minChildSize: SheetProvider.minExtent,
-      maxChildSize: SheetProvider.maxExtent,
-      initialChildSize: _sheetProvider.initialExtent,
-      builder: (ctx, scrollController) {
-        // Set ScrollController
+  Widget _draggableScrollableSheet() => GestureDetector(
+      behavior: HitTestBehavior.opaque,
+      onTap: () => Navigator.pop(context),
+      child: FractionallySizedBox(
+          heightFactor: SheetProvider.maxExtent,
+          child: DraggableScrollableSheet(
+              expand: _sheetProvider.isExpanded,
+              minChildSize: SheetProvider.minExtent,
+              maxChildSize: SheetProvider.maxExtent,
+              initialChildSize: _sheetProvider.initialExtent,
+              builder: (ctx, scrollController) {
+                // Set ScrollController
 
-        this._scrollController = scrollController;
-        return _bottomSheetBody();
-      });
+                this._scrollController = scrollController;
+                return _bottomSheetBody();
+              })));
 
-  Widget _bottomSheetBody() => Column(
-        mainAxisSize: MainAxisSize.min,
-        children: [
-          widget.tabTopBuilder?.call(context) ?? GiphyTabTop(),
-          GiphyTabBar(
-            tabController: _tabController,
-            showGIFs: widget.showGIFs,
-            showStickers: widget.showStickers,
-            showEmojis: widget.showEmojis,
-          ),
-          SearchAppBar(
-            scrollController: this._scrollController,
-            searchAppBarBuilder: widget.searchAppBarBuilder,
-          ),
-          Expanded(
-            child: GiphyTabView(
-              tabController: _tabController,
-              scrollController: this._scrollController,
-              showGIFs: widget.showGIFs,
-              showStickers: widget.showStickers,
-              showEmojis: widget.showEmojis,
-            ),
-          ),
-          widget.tabBottomBuilder?.call(context) ?? GiphyTabBottom(),
-        ],
-      );
+  Widget _bottomSheetBody() {
+    DragStartDetails? startVerticalDragDetails;
+    DragUpdateDetails? updateVerticalDragDetails;
+    return Center(
+        child: Container(
+            decoration: BoxDecoration(
+                borderRadius: const BorderRadius.only(
+                    topRight: Radius.circular(16),
+                    topLeft: Radius.circular(16)),
+                boxShadow: [
+                  BoxShadow(
+                      blurRadius: 10,
+                      spreadRadius: 16,
+                      color: widget.whiteBackground
+                          ? Colors.white.withOpacity(0.21)
+                          : Colors.black.withOpacity(0.3),
+                      offset: const Offset(0, 16))
+                ]),
+            child: ClipRRect(
+                borderRadius: const BorderRadius.only(
+                    topRight: Radius.circular(16),
+                    topLeft: Radius.circular(16)),
+                child: BackdropFilter(
+                    filter: ui.ImageFilter.blur(
+                      sigmaX: 40.0,
+                      sigmaY: 40.0,
+                    ),
+                    child: Container(
+                        decoration: BoxDecoration(
+                            color: widget.whiteBackground
+                                ? Colors.white.withOpacity(0.27)
+                                : Colors.black.withOpacity(0.5),
+                            borderRadius: const BorderRadius.only(
+                                topRight: Radius.circular(16),
+                                topLeft: Radius.circular(16)),
+                            border: Border.all(
+                              width: 1.5,
+                              color: Colors.transparent,
+                            )),
+                        child: Center(
+                            child: Column(
+                          mainAxisSize: MainAxisSize.min,
+                          children: [
+                            widget.tabTopBuilder?.call(context) ??
+                                GiphyTabTop(topDragColor: widget.topDragColor),
+                            SizedBox(
+                              height: 13,
+                            ),
+                            SearchAppBar(
+                              scrollController: this._scrollController,
+                              searchAppBarBuilder: widget.searchAppBarBuilder,
+                              textEditingController: textEditingController,
+                              focus: _focusNode,
+                            ),
+                            SizedBox(
+                              height: 10,
+                            ),
+                            Consumer<TabProvider>(
+                                builder: (tabProviderContext, tabProvider, _) {
+                              if (widget.addMediaTopWidget != null &&
+                                  textEditingController.text.isEmpty &&
+                                  !_focusNode.hasFocus) {
+                                return AnimatedContainer(
+                                    duration: Duration(milliseconds: 310),
+                                    height: tabProvider.showAddTopMediaWidgets
+                                        ? null
+                                        : 0,
+                                    child: tabProvider.showAddTopMediaWidgets
+                                        ? GestureDetector(
+                                            onVerticalDragStart: (dragDetails) {
+                                              startVerticalDragDetails =
+                                                  dragDetails;
+                                            },
+                                            onVerticalDragUpdate:
+                                                (dragDetails) {
+                                              updateVerticalDragDetails =
+                                                  dragDetails;
+                                            },
+                                            onVerticalDragEnd: (endDetails) {
+                                              if (startVerticalDragDetails !=
+                                                      null &&
+                                                  updateVerticalDragDetails !=
+                                                      null) {
+                                                double dx =
+                                                    updateVerticalDragDetails!
+                                                            .globalPosition.dx -
+                                                        startVerticalDragDetails!
+                                                            .globalPosition.dx;
+                                                double dy =
+                                                    updateVerticalDragDetails!
+                                                            .globalPosition.dy -
+                                                        startVerticalDragDetails!
+                                                            .globalPosition.dy;
+                                                double? velocity =
+                                                    endDetails.primaryVelocity;
+
+                                                //Convert values to be positive
+                                                if (dx < 0) dx = -dx;
+                                                if (dy < 0) dy = -dy;
+
+                                                if (velocity != null &&
+                                                    velocity < 0) {
+                                                  //scroll up
+                                                  if (tabProvider
+                                                      .showAddTopMediaWidgets)
+                                                    tabProvider
+                                                        .setShowAddTopMediaWidgets(
+                                                            false);
+                                                } else {
+                                                  //scroll down
+                                                  //   print("yyyyyyyyyy");
+                                                }
+                                              }
+                                            },
+                                            child: Container(
+                                                margin: EdgeInsets.only(
+                                                    top: 17, bottom: 20),
+                                                child:
+                                                    widget.addMediaTopWidget))
+                                        : SizedBox());
+                              } else {
+                                return SizedBox();
+                              }
+                            }),
+                            widget.tabBottomBuilder?.call(context) ??
+                                GiphyTabBottom(),
+                            GiphyTabBar(
+                              tabController: _tabController,
+                              showGIFs: widget.showGIFs,
+                              showStickers: widget.showStickers,
+                              showEmojis: widget.showEmojis,
+                            ),
+                            Expanded(
+                              child: GiphyTabView(
+                                tabController: _tabController,
+                                scrollController: this._scrollController,
+                                showGIFs: widget.showGIFs,
+                                showStickers: widget.showStickers,
+                                showEmojis: widget.showEmojis,
+                              ),
+                            ),
+                          ],
+                        )))))));
+  }
 }
