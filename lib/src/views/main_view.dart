149b0090c1a7822678a97e1689777b4f53fa590f
intellign <ismailarashad@aol.com>
2024-12-30 02:04:01 -0500

    Update main_view.dart

--- lib/src/views/main_view.dart
+++ lib/src/views/main_view.dart
@@ -53,6 +53,9 @@ class _MainViewState extends State<MainView>
   // Tab Controller
   late TabController _tabController;
 
+  // Scrolling Provider
+  late ScrollingProvider scrollingProvider;
+
   int lastGiphyTabSaved = 0;
 
   late TextEditingController textEditingController;
@@ -60,6 +63,8 @@ class _MainViewState extends State<MainView>
   @override
   void initState() {
     super.initState();
+    scrollingProvider = Provider.of<ScrollingProvider>(context, listen: false);
+    _scrollController.addListener(_scrollListener);
 
     textEditingController = new TextEditingController(
         text: Provider.of<AppBarProvider>(context, listen: false).queryText);
@@ -94,8 +99,35 @@ class _MainViewState extends State<MainView>
     super.didChangeDependencies();
   }
 
+  void _scrollListener() {
+    double showoffset =
+        10.0; //Back to top botton will show on scroll offset 10.0
+
+    /*
+    if (_scrollController.offset > showoffset) {
+      if (!scrollingProvider.showScrollbtn) {
+        scrollingProvider.updateScrolling(true);
+      }
+    } else {
+      if (scrollingProvider.showScrollbtn) {
+        scrollingProvider.updateScrolling(false);
+      }
+    }
+
+    */
+    if (_scrollController.offset < showoffset) {
+      if (scrollingProvider.showScrollbtn) {
+        scrollingProvider.updateScrolling(false);
+      }
+    } else if (_scrollController.position.pixels ==
+        _scrollController.position.maxScrollExtent) {
+      scrollingProvider.updateScrolling(true);
+    }
+  }
+
   @override
   void dispose() {
+    _scrollController.removeListener(_scrollListener);
     // Dispose of resources properly
     widget.sharedPreferences.setInt("lastGiphyTabSaved", _tabController.index);
     super.dispose();
@@ -123,131 +155,134 @@ class _MainViewState extends State<MainView>
                 return _bottomSheetBody();
               })));
 
-  Widget GestureWidget(
-      {required Widget child, required TabProvider tabProvider}) {
-    DragStartDetails? startVerticalDragDetails;
-    DragUpdateDetails? updateVerticalDragDetails;
-    return GestureDetector(
-        onVerticalDragStart: (dragDetails) {
-          startVerticalDragDetails = dragDetails;
-        },
-        onVerticalDragUpdate: (dragDetails) {
-          updateVerticalDragDetails = dragDetails;
-        },
-        onVerticalDragEnd: (endDetails) {
-          if (startVerticalDragDetails != null &&
-              updateVerticalDragDetails != null) {
-            double dx = updateVerticalDragDetails!.globalPosition.dx -
-                startVerticalDragDetails!.globalPosition.dx;
-            double dy = updateVerticalDragDetails!.globalPosition.dy -
-                startVerticalDragDetails!.globalPosition.dy;
-            double? velocity = endDetails.primaryVelocity;
-
-            //Convert values to be positive
-            if (dx < 0) dx = -dx;
-            if (dy < 0) dy = -dy;
-
-            if (velocity != null && velocity < 0) {
-              //scroll up
-              if (widget.addMediaTopWidget != null &&
-                  tabProvider.showAddTopMediaWidgets)
-                tabProvider.setShowAddTopMediaWidgets(false);
-            } else {
-              //scroll down
-              //   print("yyyyyyyyyy");
-            }
-          }
-        },
-        child: child);
-  }
-
   Widget _bottomSheetBody() {
-    return Center(
-      child: Stack(
-        children: [
-          Container(
-            decoration: BoxDecoration(
-              borderRadius: const BorderRadius.only(
-                topRight: Radius.circular(16),
-                topLeft: Radius.circular(16),
-              ),
-              boxShadow: [
-                BoxShadow(
-                  blurRadius: 10,
-                  spreadRadius: 16,
-                  color: widget.whiteBackground
-                      ? Colors.white.withOpacity(0.21)
-                      : Colors.black.withOpacity(0.3),
-                  offset: const Offset(0, 16),
-                ),
-              ],
-            ),
-            child: ClipRRect(
-              borderRadius: const BorderRadius.only(
-                topRight: Radius.circular(16),
-                topLeft: Radius.circular(16),
-              ),
-              child: BackdropFilter(
-                filter: ui.ImageFilter.blur(
-                  sigmaX: 10.0,
-                  sigmaY: 10.0,
-                ),
-                child: Container(
-                  decoration: BoxDecoration(
-                    color: widget.whiteBackground
-                        ? Colors.white.withOpacity(0.27)
-                        : Colors.black.withOpacity(0.5),
-                    borderRadius: const BorderRadius.only(
-                      topRight: Radius.circular(16),
-                      topLeft: Radius.circular(16),
+    return Scaffold(
+        backgroundColor: Colors.transparent,
+        floatingActionButton: Consumer<ScrollingProvider>(
+            builder: (tabProviderContext, scrollingProvider, _) =>
+                !scrollingProvider.showScrollbtn
+                    ? const SizedBox()
+                    : FloatingActionButton(
+                        heroTag: "giphy_get_scroll_to_top",
+                        backgroundColor: Colors.white,
+                        elevation: 11,
+                        mini: scrollingProvider.showScrollbtn,
+                        child: Icon(
+                          Icons.arrow_drop_up_rounded,
+                          size: 35,
+                          color: Colors.red,
+                        ),
+                        onPressed: scrollingProvider.showScrollbtn
+                            ? () {
+                                _scrollController.animateTo(
+                                    //go to top of scroll
+                                    0, //scroll offset to go
+                                    duration: Duration(
+                                        milliseconds: 500), //duration of scroll
+                                    curve: Curves.fastOutSlowIn //scroll type
+                                    );
+                              }
+                            : null)),
+        body: Center(
+          child: Stack(
+            children: [
+              Container(
+                decoration: BoxDecoration(
+                  borderRadius: const BorderRadius.only(
+                    topRight: Radius.circular(16),
+                    topLeft: Radius.circular(16),
+                  ),
+                  boxShadow: [
+                    BoxShadow(
+                      blurRadius: 10,
+                      spreadRadius: 16,
+                      color: widget.whiteBackground
+                          ? Colors.white.withOpacity(0.21)
+                          : Colors.black.withOpacity(0.3),
+                      offset: const Offset(0, 16),
                     ),
-                    border: Border.all(
-                      width: 1.5,
-                      color: Colors.transparent,
+                  ],
+                ),
+                child: ClipRRect(
+                  borderRadius: const BorderRadius.only(
+                    topRight: Radius.circular(16),
+                    topLeft: Radius.circular(16),
+                  ),
+                  child: BackdropFilter(
+                    filter: ui.ImageFilter.blur(
+                      sigmaX: 10.0,
+                      sigmaY: 10.0,
+                    ),
+                    child: Container(
+                      decoration: BoxDecoration(
+                        color: widget.whiteBackground
+                            ? Colors.white.withOpacity(0.27)
+                            : Colors.black.withOpacity(0.5),
+                        borderRadius: const BorderRadius.only(
+                          topRight: Radius.circular(16),
+                          topLeft: Radius.circular(16),
+                        ),
+                        border: Border.all(
+                          width: 1.5,
+                          color: Colors.transparent,
+                        ),
+                      ),
                     ),
                   ),
                 ),
               ),
-            ),
-          ),
-          Center(
-            child: Column(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                widget.tabTopBuilder?.call(context) ??
-                    GiphyTabTop(topDragColor: widget.topDragColor),
-                SizedBox(height: 13),
-                SearchAppBar(
-                  scrollController: this._scrollController,
-                  searchAppBarBuilder: widget.searchAppBarBuilder,
-                  textEditingController: textEditingController,
-                  focus: _focusNode,
-                ),
-                SizedBox(height: 3.7),
-                Expanded(
-                  child: NestedScrollView(
-                    controller: _scrollController,
-                    floatHeaderSlivers: true,
-                    headerSliverBuilder: (context, innerBoxIsScrolled) => [
-                      SliverToBoxAdapter(
-                          child: Container(
-                              // duration: const Duration(milliseconds: 310),
-                              child: Column(children: [
-                        if (widget.addMediaTopWidget != null &&
-                            textEditingController.text.isEmpty)
-                          widget.addMediaTopWidget!,
-                        widget.tabBottomBuilder?.call(context) ??
-                            GiphyTabBottom(),
-                        GiphyTabBar(
-                          tabController: _tabController,
-                          showGIFs: widget.showGIFs,
-                          showStickers: widget.showStickers,
-                          showEmojis: widget.showEmojis,
-                        ),
-                      ])))
-                    ],
-                    body:
-                        /* NotificationListener<ScrollNotification>(
+              Center(
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    widget.tabTopBuilder?.call(context) ??
+                        GiphyTabTop(topDragColor: widget.topDragColor),
+                    SizedBox(height: 13),
+                    SearchAppBar(
+                      scrollController: this._scrollController,
+                      searchAppBarBuilder: widget.searchAppBarBuilder,
+                      textEditingController: textEditingController,
+                      focus: _focusNode,
+                    ),
+                    SizedBox(height: 3.7),
+                    Expanded(
+                      child: NestedScrollView(
+                        controller: _scrollController,
+                        floatHeaderSlivers: true,
+                        headerSliverBuilder: (context, innerBoxIsScrolled) => [
+                          SliverToBoxAdapter(
+                              child: Container(
+                                  // duration: const Duration(milliseconds: 310),
+                                  child: Column(children: [
+                            if (widget.addMediaTopWidget != null &&
+                                textEditingController.text.isEmpty)
+                              Consumer<ScrollingProvider>(
+                                  builder: (tabProviderContext,
+                                          scrollingProvider, _) =>
+                                      AnimatedContainer(
+                                          duration:
+                                              const Duration(milliseconds: 590),
+                                          height:
+                                              scrollingProvider.scrolledToTop ==
+                                                          true ||
+                                                      scrollingProvider
+                                                              .scrolledToTop ==
+                                                          null
+                                                  ? null
+                                                  : 0,
+                                          child: widget.addMediaTopWidget!)),
+                            widget.tabBottomBuilder?.call(context) ??
+                                GiphyTabBottom(),
+                            GiphyTabBar(
+                              tabController: _tabController,
+                              showGIFs: widget.showGIFs,
+                              showStickers: widget.showStickers,
+                              showEmojis: widget.showEmojis,
+                            ),
+                          ])))
+                        ],
+                        body:
+                            /* NotificationListener<ScrollNotification>(
                       onNotification: (scrollNotification) {
                         if (scrollNotification is ScrollEndNotification) {
                           if (_scrollController.position.extentAfter == 0) {
@@ -260,21 +295,21 @@ class _MainViewState extends State<MainView>
                       },
                       child:
                       */
-                        GiphyTabView(
-                      tabController: _tabController,
-                      scrollController: _scrollController,
-                      showGIFs: widget.showGIFs,
-                      showStickers: widget.showStickers,
-                      showEmojis: widget.showEmojis,
+                            GiphyTabView(
+                          tabController: _tabController,
+                          scrollController: _scrollController,
+                          showGIFs: widget.showGIFs,
+                          showStickers: widget.showStickers,
+                          showEmojis: widget.showEmojis,
+                        ),
+                        // ),
+                      ),
                     ),
-                    // ),
-                  ),
+                  ],
                 ),
-              ],
-            ),
+              ),
+            ],
           ),
-        ],
-      ),
-    );
+        ));
   }
 }
